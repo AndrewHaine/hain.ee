6d339aab28914368bbc2866642f2ecf3c10a1ef0
Andrew Haine <andrewhaineweb@gmail.com>
2017-10-22 17:00:00 +0100

    Create a directory for storing .well-known

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Support /.well-known (required for adding SSL with certbot)
-app.use(express.static(path.join(__dirname, '.well-known'), {dotfiles: 'allow'}));
+app.use(express.static(path.join(__dirname, 'ssl'), {dotfiles: 'allow'}));
 
 // Always wear a helmet
 app.use(helmet());
