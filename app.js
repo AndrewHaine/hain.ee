58ae1808116608a40d20285f0890f4e3df87660b
Andrew Haine <andrewhaineweb@gmail.com>
2017-11-02 21:04:48 +0000

    Force correct domain in production

--- app.js
+++ app.js
@@ -29,6 +29,20 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Force rewrite to production domain
+app.use((req, res, next) => {
+  if(process.env.ENV === 'production' && process.env.PRODUCTION_DOMAIN) {
+    const domain = process.env.PRODUCTION_DOMAIN;
+    if(req.hostname !== domain) {
+      res.redirect('https://' + domain);
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
 // Redirect all http request to https
 app.set('forceSSLOptions', {
   httpsPort: process.env.PORT
